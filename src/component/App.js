9ac85491cd2dddfa35a4289eb1b5dda30464e47d
Md Ahosan Habib <cseahosan@gmail.com>
2019-05-04 17:59:25 +0600

    usage of state example 2

--- src/component/App.js
+++ src/component/App.js
@@ -4,11 +4,19 @@ import First from './First/First'
 import Counter from './Counter/Counter'
 
 class App extends Component {
+
+    state = {
+        persons: [
+            { name: 'Ahosan Habib', email: 'mdahosahabib@outlook.com', address: 'Dhaka, Bangladesh' },
+            { name: 'Ahosan', email: 'cseahosan@gmaill.com', address: 'Dhaka, Bangladesh' },
+            { name: 'Habib', email: 'ahosan.laravel@gmail.com', address: 'Dhaka, Bangladesh' }
+        ]
+    }
+
   render() {
     return (
       <div className="App">
-          {/*<First name='Ahosan Habib' email='mdahosanhabib@outlook.com' address='Dhaka, Bangladesh'/>*/}
-          <Counter/>
+          <First name={ this.state.persons[0].name } email={ this.state.persons[0].email } address={this.state.persons[0].address}/>
     </div>
     );
   }
