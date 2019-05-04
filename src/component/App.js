0f62fec777c7ac7d04880d0a64140b1f9c60eedf
Md Ahosan Habib <cseahosan@gmail.com>
2019-05-04 16:23:10 +0600

    usage of props

--- src/component/App.js
+++ src/component/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <First/>
+          <First name='Ahosan Habib' email='mdahosanhabib@outlook.com' address='Dhaka, Bangladesh'/>
       </div>
     );
   }
--- src/component/First/First.js
+++ src/component/First/First.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 
 class First extends Component {
     render() {
-        return <h1>I am a brand new component</h1>
+        return (
+            <div>
+                <h3>Name : {this.props.name}</h3>
+                <p>Email : {this.props.email}</p>
+                <address>Address : {this.props.address}</address>
+            </div>
+        )
     }
 }
 
