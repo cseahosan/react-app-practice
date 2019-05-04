0f9a5db286881ecc59a0cec12aff406507c4325a
Md Ahosan Habib <cseahosan@gmail.com>
2019-05-04 16:08:44 +0600

    add component directory and create first component

---
+++ src/component/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import First from './First/First'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <First/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/component/First/First.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class First extends Component {
+    render() {
+        return <h1>I am a brand new component</h1>
+    }
+}
+
+export default First
