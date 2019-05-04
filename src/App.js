f0e248d9d05aed4269df971a60ed75e9fa26ad71
Ahosan Habib <cseahosan@gmail.com>
2019-05-04 14:03:54 +0600

    use variable in jsx

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import './App.css';
 
 class App extends Component {
   render() {
+
+    let check = true;
+    let h2 = 'Welcome to React-JS'
+    let p = 'To get started, edit <code>src/App.js</code> and save to reload.'
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React-JS</h2>
+          <h2>{ h2 }</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          { p }
         </p>
+        <div>{ check ? 'Yes' : 'No' }</div>
       </div>
     );
   }
